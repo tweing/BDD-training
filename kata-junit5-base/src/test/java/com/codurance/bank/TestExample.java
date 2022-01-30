4bfeeb772c8bada3aaa8c2c0fc23236fc1167dac
Javier Chacana <javier.chacana@codurance.com>
2022-01-30 22:22:03 +0100

    Uploading base junit5 project

---
+++ kata-junit5-base/src/test/java/com/codurance/bank/TestExample.java
@@ -0,0 +1,13 @@
+package com.codurance.bank;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestExample {
+
+    @Test
+    void this_is_an_example() {
+        assertThat(true ).isEqualTo(true);
+    }
+}
